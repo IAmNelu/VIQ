56d872bf689931929f5efd52005f020409927601
Nelu <ifrimvale@gmail.com>
2019-06-28 16:24:25 +0200

    Commenti python

--- PythonScripts/01a_Snellisci_VRC.py
+++ PythonScripts/01a_Snellisci_VRC.py
@@ -1,12 +1,14 @@
 import csv
 
 pathFileVRC = './00_f_VRC2017.csv'
-
+#apre il file csv e crea un csv reader
 with open(pathFileVRC) as fileVRC, open('01a_f_VRC_min.csv', 'w', newline='') as csv_dest:
     csv_reader = csv.reader(fileVRC, delimiter=';')
     csv_writer = csv.writer(csv_dest, delimiter=';')
     line_count = 0
     for row in csv_reader:
+        # elabora ogni singola riga dal file letto e
+        # salva nel csv.writer solo alcuni campi delprecedente CSV
         csv_writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[12]])
         line_count += 1
 
--- PythonScripts/03_Converter.py
+++ PythonScripts/03_Converter.py
@@ -9,7 +9,8 @@ if len(sys.argv) != 3:
 
 path = sys.argv[1]
 name = sys.argv[2]
-
+# i tre parametri che prende da linea di comando sono il path di un file csv e il nome del file 
+# json di desinazione
 with open(path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     line_count = 0
@@ -20,16 +21,16 @@ with open(path) as csv_file:
         single_line_data = {}
         if line_count == 0:
             for element in row:
-                headers.append(element)
+                headers.append(element) # popola l'array di intestazioni
             line_count += 1
         else:
-            for header, value in zip(headers, row):
-                single_line_data[header] = value
-            data.append(single_line_data)
+            for header, value in zip(headers, row): # loop sull'array creato da zip con header e riga
+                single_line_data[header] = value # popola il singolo oggetto json
+            data.append(single_line_data) # aggiunge l'oggetto all'array
             line_count += 1
-    json_data = json.dumps(data)
+    json_data = json.dumps(data) # crea un oggetto json dall'array
     with open(name, 'w') as outfile:
-        json.dump(data, outfile)
-    print('Processed ' + str(line_count) + ' lines.')
+        json.dump(data, outfile) # scrive il json sul file
+    print('Processed ' + str(line_count) + ' lines.') #statistiche di compressione
 
 
